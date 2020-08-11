d8b18ad1a840308e941e85843a84339f22c42618
Charles Milette <charles.milette@gmail.com>
2020-08-11 16:37:43 -0400

    JavaScript sucks

--- build.js
+++ build.js
@@ -2,13 +2,17 @@ const Discord = require("discord.js");
 const fs = require("fs");
 const path = require("path");
 
-// Make sure the bot connected to the gateway at least once.
-const client = new Discord.Client();
-client.on("ready", () => client.destroy());
-client.login(process.env.DISCORD_BOT_TOKEN);
+async function main() {
+    // Make sure the bot connected to the gateway at least once.
+    const client = new Discord.Client();
+    await client.login(process.env.DISCORD_BOT_TOKEN);
+    client.destroy();
 
-if (!process.env.USE_NETLIFY_FORMS) {
-    const content = await fs.readFile(path.resolve(__dirname, "public", "form.html"), "UTF-8");
+    if (!process.env.USE_NETLIFY_FORMS) {
+        const content = await fs.readFile(path.resolve(__dirname, "public", "form.html"), "UTF-8");
 
-    content.replace("action=\"/success\" netlify", "action=\"/.netlify/functions/submission-created\"");
+        content.replace("action=\"/success\" netlify", "action=\"/.netlify/functions/submission-created\"");
+    }
 }
+
+main();
