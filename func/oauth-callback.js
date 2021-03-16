f27950f4433a8eef58dc32d2976c41496fc801b5
Charles Milette <charles.milette@gmail.com>
2021-03-15 20:02:23 -0400

    Try stripping the original query string

--- func/oauth-callback.js
+++ func/oauth-callback.js
@@ -36,7 +36,7 @@ exports.handler = async function (event, context) {
                 return {
                     statusCode: 303,
                     headers: {
-                        "Location": "/error"
+                        "Location": "/error?"
                     }
                 };
             }
