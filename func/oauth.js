38c4ec158003888d5b82ecf47bff19c2d45e389f
Charles Milette <charles.milette@gmail.com>
2021-08-29 02:06:19 -0400

    Use prime URL

--- func/oauth.js
+++ func/oauth.js
@@ -1,5 +1,5 @@
 exports.handler = async function (event, context) {
-    const redirectUri = new URL("/.netlify/functions/oauth-callback", process.env.URL);
+    const redirectUri = new URL("/.netlify/functions/oauth-callback", process.env.DEPLOY_PRIME_URL);
     let url = `https://discord.com/api/oauth2/authorize?client_id=${encodeURIComponent(process.env.DISCORD_CLIENT_ID)}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=code&scope=identify&prompt=none`;
 
     if (event.queryStringParameters.state !== undefined) {
