3a4cb80663cda34aa15786800b462b9da700446a
Charles Milette <charles.milette@gmail.com>
2021-08-30 18:35:38 -0400

    Fix build script

--- build.js
+++ build.js
@@ -10,27 +10,35 @@ function assertSuccess(err) {
     }
 }
 
-function replaceInFile(file, original, replacement) {
+function replaceInFile(file, original, replacement, callback) {
     fs.readFile(file, "UTF-8", (err, data) => {
         assertSuccess(err);
 
-        fs.writeFile(file, data.replace(original, replacement), "UTF-8", assertSuccess);
+        fs.writeFile(file, data.replace(original, replacement), "UTF-8", err => {
+            assertSuccess(err);
+
+            if (callback) {
+                callback();
+            }
+        });
     });
 }
 
 async function main() {
-    const url = process.env.CONTEXT === "production" ? process.env.URL : process.env.DEPLOY_PRIME_URL;
-    replaceInFile(path.resolve(__dirname, "func", "oauth.js"), "DEPLOY_PRIME_URL", `"${url}"`);
-    replaceInFile(path.resolve(__dirname, "func", "oauth-callback.js"), "DEPLOY_PRIME_URL", `"${url}"`);
-    replaceInFile(path.resolve(__dirname, "func", "submission-created.js"), "DEPLOY_PRIME_URL", `"${url}"`);
+    const func = path.resolve(__dirname, "func");
 
-    if (!process.env.USE_NETLIFY_FORMS) {
-        fs.rename(path.resolve(__dirname, "func", "submission-created.js"), path.resolve(__dirname, "func", "submit-appeal.js"), assertSuccess);
-        replaceInFile(path.resolve(__dirname, "public", "form.html"), "action=\"/success\" netlify", "action=\"/.netlify/functions/submit-appeal\"");
-    }
+    const url = process.env.CONTEXT === "production" ? process.env.URL : process.env.DEPLOY_PRIME_URL;
+    replaceInFile(path.resolve(func, "oauth.js"), "DEPLOY_PRIME_URL", `"${url}"`);
+    replaceInFile(path.resolve(func, "oauth-callback.js"), "DEPLOY_PRIME_URL", `"${url}"`);
+    replaceInFile(path.resolve(func, "submission-created.js"), "DEPLOY_PRIME_URL", `"${url}"`, () => {
+        if (!process.env.USE_NETLIFY_FORMS) {
+            fs.rename(path.resolve(func, "submission-created.js"), path.resolve(func, "submit-appeal.js"), assertSuccess);
+            replaceInFile(path.resolve(__dirname, "public", "form.html"), "action=\"/success\" netlify", "action=\"/.netlify/functions/submit-appeal\"");
+        }
+    });
 
     if (process.env.DISABLE_UNBAN_LINK) {
-        fs.unlink(path.resolve(__dirname, "func", "unban.js"), assertSuccess);
+        fs.unlink(path.resolve(func, "unban.js"), assertSuccess);
     }
 
     // Make sure the bot connected to the gateway at least once.
