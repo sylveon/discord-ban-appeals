71c3db38fd14591922c59d609bd4fc2fede9cc97
Charles Milette <charles.milette@gmail.com>
2022-02-26 23:45:46 -0500

    Yay for outdated versions of node

--- build.js
+++ build.js
@@ -17,7 +17,7 @@ function replaceInFile(file, original, replacement, callback) {
     fs.readFile(file, "UTF-8", (err, data) => {
         assertSuccess(err);
 
-        fs.writeFile(file, data.replaceAll(original, replacement), "UTF-8", err => {
+        fs.writeFile(file, data.replace(original, replacement), "UTF-8", err => {
             assertSuccess(err);
 
             if (callback) {
@@ -31,7 +31,7 @@ async function main() {
     const func = path.resolve(__dirname, "func");
 
     const url = process.env.CONTEXT === "production" ? process.env.URL : process.env.DEPLOY_PRIME_URL;
-    replaceInFile(path.resolve(func, "oauth.js"), "DEPLOY_PRIME_URL", `"${url}"`);
+    replaceInFile(path.resolve(func, "oauth.js"), /DEPLOY_PRIME_URL/g, `"${url}"`);
     replaceInFile(path.resolve(func, "oauth-callback.js"), "DEPLOY_PRIME_URL", `"${url}"`);
     replaceInFile(path.resolve(func, "submission-created.js"), "DEPLOY_PRIME_URL", `"${url}"`, () => {
         if (!process.env.USE_NETLIFY_FORMS) {
